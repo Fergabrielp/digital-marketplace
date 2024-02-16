5ca53372ab84d0b740041d961e93c15b16e5be68
Your Name <perezfernando95@gmail.com>
2024-02-16 04:29:17 -0300

    Cart UI

---
+++ src/config/index.ts
@@ -0,0 +1,44 @@
+export const PRODUCT_CATEGORIES = [
+  {
+    label: "UI Kits",
+    value: "ui_kits" as const,
+    featured: [
+      {
+        name: "Editor Picks",
+        href: "#",
+        imagSrc: "/nav/ui-kits/mixed.jpg",
+      },
+      {
+        name: "New  Arrivals",
+        href: "#",
+        imagSrc: "/nav/ui-kits/blue.jpg",
+      },
+      {
+        name: "Bestsellers",
+        href: "#",
+        imagSrc: "/nav/ui-kits/purple.jpg",
+      },
+    ],
+  },
+  {
+    label: "Icons",
+    value: "icons" as const,
+    featured: [
+      {
+        name: "Favorite Icon Picks",
+        href: "#",
+        imagSrc: "/nav/icons/picks.jpg",
+      },
+      {
+        name: "New  Arrivals",
+        href: "#",
+        imagSrc: "/nav/icons/new.jpg",
+      },
+      {
+        name: "Bestselling Icons",
+        href: "#",
+        imagSrc: "/nav/icons/bestsellers.jpg",
+      },
+    ],
+  },
+];
